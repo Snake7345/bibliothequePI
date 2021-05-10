fcadd73ad5b583813f244f32b91beab2df16fd62
Snake7345 <thebasketman7@hotmail.com>
2021-05-10 16:10:15 +0200

    v0.0.73 Ajout d'un formulaire de recherche (a modifier), ajout de la librairie PDFTools qui permet de faire les factures et des fichiers de l'ancien projet et ajout dans les messages.properties

---
+++ src/main/java/pdfTools/Encadrement.java
@@ -0,0 +1,40 @@
+package pdfTools;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+
+public class Encadrement 
+{
+	
+	public static void creation(PDPageContentStream cs, int x, int y, int lg, int ht) throws IOException
+	{
+		// Création d'un encadrement
+		//ligne supérieure
+		cs.setLineWidth(1);
+		cs.moveTo(x, y);
+		cs.lineTo(x+lg, y);
+		cs.closeAndStroke();
+		
+		// ligne inférieure
+		cs.setLineWidth(1);
+		cs.moveTo(x, y-ht);
+		cs.lineTo(x + lg, y-ht);
+		cs.closeAndStroke();
+		
+		// ligne verticale gauche
+		cs.setLineWidth(1);
+		cs.moveTo(x, y);
+		cs.lineTo(x, y-ht);
+		cs.closeAndStroke();
+		
+		// ligne verticale droite
+		cs.setLineWidth(1);
+		cs.moveTo(x+lg, y);
+		cs.lineTo(x+lg, y -ht);
+		cs.closeAndStroke();
+		
+	}
+	
+	
+}
