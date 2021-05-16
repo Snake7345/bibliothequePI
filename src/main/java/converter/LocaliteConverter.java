01e576b6ce875d29d74df66bedc760e1626b5267
Snake7345 <thebasketman7@hotmail.com>
2021-05-16 21:47:38 +0200

    v0.0.108 Mise a jour localiteConverter (probleme)

---
+++ src/main/java/converter/LocaliteConverter.java
@@ -0,0 +1,41 @@
+package converter;
+
+import entities.Localites;
+import entities.Roles;
+import org.apache.log4j.Logger;
+import services.SvcRoles;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Named;
+
+
+@Named
+@FacesConverter(value = "localiteConverter")
+public class LocaliteConverter implements Converter {
+    private static final Logger log = Logger.getLogger(LocaliteConverter.class);
+    private final SvcRoles service = new SvcRoles();
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        if (s != null && s.trim().length() > 0) {
+            int id = Integer.parseInt(s);
+
+            return service.getById(id);
+        } else
+            return null;
+
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) {
+        if (o != null) {
+            log.debug("test vallues " + o.toString());
+            return String.valueOf(((Localites) o).getIdLocalites());
+        }
+        else
+            return null;
+    }
+}
