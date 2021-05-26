5ab0330abc30fc0a86fd95abd96be5bf10c96a4e
Snake7345 <thebasketman7@hotmail.com>
2021-05-26 17:32:03 +0200

    v0.0.143 mise a jourr des validateurs sur certaines variables et des todo

---
+++ src/main/java/validators/NomPrenomValidator.java
@@ -0,0 +1,40 @@
+package validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("rawtypes")
+@FacesValidator("nomPrenomValidator")
+
+public class NomPrenomValidator implements Validator{
+
+    private static final String EMAIL_PATTERN = "^[a-zA-Zéèàùûêâôëç]{1}[a-zA-Zéèàùûêâôëç \\'-]*[a-zA-Zéèàùûêâôëç]$";
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public NomPrenomValidator()
+    {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, 	Object value)
+            throws ValidatorException {
+
+        matcher = pattern.matcher(value.toString());
+        if(!matcher.matches()){
+
+            FacesMessage msg = new FacesMessage("Le nom de famille n'est pas valide, uniquelement lettre et caractère accentué.","Le nom de famille n'est pas valide, uniquelement lettre et caractère accentué.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+
+    }
+}
