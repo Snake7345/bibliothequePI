c142be9b5a2d2122880da9efe19f4d862f940e2a
Snake7345 <thebasketman7@hotmail.com>
2021-05-29 00:17:36 +0200

    v0.0.160 formEtatLivre.xhtml en cours

--- src/main/java/managedBean/ExemplairesLivresEnumBean.java
+++ src/main/java/managedBean/ExemplairesLivresEnumBean.java
@@ -2,13 +2,17 @@ package managedBean;
 
 import enumeration.ExemplairesLivresEtatEnum;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 
+@Named
+@SessionScoped
 public class ExemplairesLivresEnumBean implements Serializable{
     // Déclaration des variables globales
         private static final long serialVersionUID = 1L;
 
-        public ExemplairesLivresEtatEnum[] getSexeEnum()
+        public ExemplairesLivresEtatEnum[] getEtatEnum()
         {
             return ExemplairesLivresEtatEnum.values();
         }
--- src/main/java/managedBean/FactureEnumBean.java
+++ src/main/java/managedBean/FactureEnumBean.java
@@ -2,8 +2,12 @@ package managedBean;
 
 import enumeration.FactureEtatEnum;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 
+@Named
+@SessionScoped
 public class FactureEnumBean implements Serializable{
 
         private static final long serialVersionUID = 1L;
