d61a85a519e8869df8f6e8252332c803f74696f0
Snake7345 <thebasketman7@hotmail.com>
2021-05-29 16:46:38 +0200

    v0.0.174 Validation UtilisateurExistValidator fonctionne

--- src/main/java/validators/UtilisateurExistValidator.java
+++ src/main/java/validators/UtilisateurExistValidator.java
@@ -1,4 +1,4 @@
-/*package validators;
+package validators;
 
 
 import services.SvcUtilisateurs;
@@ -7,14 +7,14 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import javax.validation.Validator;
 
-@FacesValidator("utilisateursExists")
-public class UtilisateursExists implements Validator
+@FacesValidator("utilisateurExistValidator")
+public class UtilisateurExistValidator implements Validator
 {
 
-    public UtilisateursExists()
+    public UtilisateurExistValidator()
     {
 
     }
@@ -22,12 +22,14 @@ public class UtilisateursExists implements Validator
     @Override
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
         String login = (String) o;
+        FacesMessage msg = new FacesMessage("Le pseudo est deja utilise, veuillez en choisir un autre");
 
         SvcUtilisateurs serviceU = new SvcUtilisateurs();
 
         try {
-            if (serviceU.findByLogin(login) != null)
-                throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, message.translate(login + " est dÃ©jÃ  utilisÃ©"), null));
+            if (!serviceU.findByLogin(login).isEmpty())
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
 
         } finally {
             serviceU.close();
@@ -36,4 +38,3 @@ public class UtilisateursExists implements Validator
 
 
 }
-*/
